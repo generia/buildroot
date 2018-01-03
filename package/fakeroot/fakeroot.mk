980c1048d0a46ab5771bdc4c5e8c1c11b896101e
alex <alex@generia.de>
2018-01-03 19:47:25 +0100

    upgrade to fakeroot 3.3, macos version

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -4,17 +4,31 @@
 #
 ################################################################################
 
-FAKEROOT_VERSION = 1.20.2
-FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
-FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20141005T221953Z/pool/main/f/fakeroot
+#FAKEROOT_VERSION = 1.20.2
+#FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
+#FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20141005T221953Z/pool/main/f/fakeroot
+
+FAKEROOT_VERSION = 3.3
+FAKEROOT_SOURCE = macosx-v$(FAKEROOT_VERSION).tar.gz
+FAKEROOT_SITE = https://github.com/mackyle/fakeroot/archive
 
 HOST_FAKEROOT_DEPENDENCIES = host-acl
+
+define HOST_FAKEROOT_BOOTSTRAP_HOOK
+	@$(call MESSAGE,"Bootstrapping")
+	if [ ! -f $($(PKG)_SRCDIR)configure ]; then (cd $($(PKG)_SRCDIR); ./bootstrap); else echo "bootstrapping already done, skipping ..."; fi;
+endef
+
+HOST_FAKEROOT_PRE_CONFIGURE_HOOKS += HOST_FAKEROOT_BOOTSTRAP_HOOK
+
 # Force capabilities detection off
 # For now these are process capabilities (faked) rather than file
 # so they're of no real use
 HOST_FAKEROOT_CONF_ENV = \
 	ac_cv_header_sys_capability_h=no \
-	ac_cv_func_capset=no
+	ac_cv_func_capset=no \
+	CFLAGS="-pipe -O2 -arch x86_64 -arch i386 -Wno-deprecated-declarations"
+		
 
 FAKEROOT_LICENSE = GPL-3.0+
 FAKEROOT_LICENSE_FILES = COPYING
