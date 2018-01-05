482f8fceab6dc60f60dd112bc560c3425bae8082
alex <alex@generia.de>
2018-01-05 15:12:13 +0100

    added install for cdrtools mkisofs

--- package/cdrtools/cdrtools.mk
+++ package/cdrtools/cdrtools.mk
@@ -1,6 +1,10 @@
 ################################################################################
 #
-# Cdrtools
+# Cdrtools (mkisoimage)
+#
+# NOTE: 
+#   This builds only "mkisofs" as a drop-in replacement of the outdated
+#   Cdrkit genisoimage.
 #
 ################################################################################
 
@@ -9,14 +13,15 @@ CDRTOOLS_SOURCE = cdrtools-$(CDRTOOLS_VERSION).tar.gz
 CDRTOOLS_SITE = https://downloads.sourceforge.net/project/cdrtools
 
 define HOST_CDRTOOLS_BUILD_CMDS
-	echo "build cdrtools ..."
-	$(HOST_MAKE_ENV) $(HOST_CDRTOOLS_MAKE_ENV) $(MAKE) $(HOST_CDRTOOLS_MAKE_OPTS) -C $(HOST_CDRTOOLS_SRCDIR)
+	# build all (NOTE: parallel make is failing)
+	$(HOST_MAKE_ENV) $(HOST_CDRTOOLS_MAKE_ENV) make -C $(HOST_CDRTOOLS_SRCDIR)
 endef
 
 define HOST_CDRTOOLS_INSTALL_CMDS
-	echo "install cdrtools ..."
-	# TODO: 
-	echo "$(INSTALL) -m 0755 $(@D)/stm32*.bin $(HOST_DIR)"
+	# install mkisofs only
+	$(HOST_MAKE_ENV) $(HOST_CDRTOOLS_MAKE_ENV) $(MAKE) INS_BASE=$(HOST_DIR) install -C $(HOST_CDRTOOLS_SRCDIR)mkisofs
+	# link to genisoimage
+	ln -s $(HOST_DIR)/bin/mkisofs $(HOST_DIR)/bin/genisoimage
 endef
 
 CDRTOOLS_LICENSE = GPL-2.0
