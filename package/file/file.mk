00e658cb3711e771d584ff863441e1a4898a1b64
alex <alex@generia.de>
2018-01-05 01:13:38 +0100

    added futher patches and makefile fixes

--- package/file/file.mk
+++ package/file/file.mk
@@ -13,5 +13,11 @@ FILE_INSTALL_STAGING = YES
 FILE_LICENSE = BSD-2-Clause, BSD-4-Clause (one file), BSD-3-Clause (one file)
 FILE_LICENSE_FILES = COPYING src/mygetopt.h src/vasprintf.c
 
+define HOST_FILE_ADD_HOSTBUILD_MARKER
+	touch $(HOST_FILE_SRCDIR)src/.host-build-marker
+endef
+
+HOST_FILE_POST_CONFIGURE_HOOKS += HOST_FILE_ADD_HOSTBUILD_MARKER
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
