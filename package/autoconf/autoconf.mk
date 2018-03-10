f56a7aa2a69fb00ca137bf010167e952fab2bd87
alex <alex@generia.de>
2018-03-10 01:27:02 +0100

    added support for gettext-autopoint in autoreconf

--- package/autoconf/autoconf.mk
+++ package/autoconf/autoconf.mk
@@ -23,4 +23,8 @@ $(eval $(host-autotools-package))
 # variables used by other packages
 AUTOCONF = $(HOST_DIR)/bin/autoconf
 AUTOHEADER = $(HOST_DIR)/bin/autoheader
-AUTORECONF = $(HOST_CONFIGURE_OPTS) ACLOCAL="$(ACLOCAL)" AUTOCONF="$(AUTOCONF)" AUTOHEADER="$(AUTOHEADER)" AUTOMAKE="$(AUTOMAKE)" AUTOPOINT=/bin/true $(HOST_DIR)/bin/autoreconf -f -i -I "$(ACLOCAL_DIR)" -I "$(ACLOCAL_HOST_DIR)"
+
+# check for autopoint that is coming from host-gettext
+AUTOPOINT = $(shell which $(HOST_DIR)/bin/autopoint || type -p $(HOST_DIR)/bin/autopoint || echo /bin/true)
+
+AUTORECONF = $(HOST_CONFIGURE_OPTS) ACLOCAL="$(ACLOCAL)" AUTOCONF="$(AUTOCONF)" AUTOHEADER="$(AUTOHEADER)" AUTOMAKE="$(AUTOMAKE)" AUTOPOINT=$(AUTOPOINT) $(HOST_DIR)/bin/autoreconf -f -i -I "$(ACLOCAL_DIR)" -I "$(ACLOCAL_HOST_DIR)"
